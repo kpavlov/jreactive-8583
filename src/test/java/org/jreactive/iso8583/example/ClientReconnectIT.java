8b3d8c188cdf28f25af03f1a8b60684bfbbf83d5
kpavlov <git@konstantinpavlov.net>
2015-07-06 01:12:07 +0300

    ClientReconnectIT: increase reconnect timeout

--- src/test/java/org/jreactive/iso8583/example/ClientReconnectIT.java
+++ src/test/java/org/jreactive/iso8583/example/ClientReconnectIT.java
@@ -17,7 +17,7 @@ public class ClientReconnectIT extends AbstractIT {
         assertThat(client.isConnected(), is(false));
         server.start();
         assertThat("server started", server.isStarted(), is(true));
-        Thread.sleep(3000L);
+        Thread.sleep(5000L);
         assertThat("client re-connected", client.isConnected(), is(true));
     }
 }
