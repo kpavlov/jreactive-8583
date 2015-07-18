c7d66f03fc69199952ae20ba646f2725dfed0ffa
kpavlov <git@konstantinpavlov.net>
2015-07-18 22:54:02 +0300

    print field names to log

--- src/main/java/org/jreactive/iso8583/netty/pipeline/IsoMessageLoggingHandler.java
+++ src/main/java/org/jreactive/iso8583/netty/pipeline/IsoMessageLoggingHandler.java
@@ -6,11 +6,46 @@ import io.netty.channel.ChannelHandler;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * ChannelHandler responsible for logging messages.
+ * <p>
+ * According to PCI DSS, sensitive cardholder data should not be exposed.
+ * When running in secure mode, ensitive cardholder data will be printed masked.
+ */
 @ChannelHandler.Sharable
 public class IsoMessageLoggingHandler extends LoggingHandler {
 
-    public IsoMessageLoggingHandler(LogLevel level) {
+    private static String[] FIELD_NAMES = new String[128];
+
+    static {
+        try (InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("org/jreactive/iso8583/iso8583fields.properties")) {
+            final Properties properties = new Properties();
+            properties.load(stream);
+            properties.forEach((key, value) -> {
+                int field = Integer.parseInt((String) key);
+                FIELD_NAMES[field - 1] = (String) value;
+            });
+        } catch (IOException | NumberFormatException e) {
+            throw new Error("Unable to load ISO8583 field descriptions", e);
+        }
+    }
+
+    private final boolean printSensitiveData;
+    private final boolean printFieldDescriptions;
+
+
+    public IsoMessageLoggingHandler(LogLevel level, boolean printSensitiveData, boolean printFieldDescriptions) {
         super(level);
+        this.printSensitiveData = printSensitiveData;
+        this.printFieldDescriptions = printFieldDescriptions;
+    }
+
+    public IsoMessageLoggingHandler(LogLevel level) {
+        this(level, true, true);
     }
 
     @Override
@@ -22,18 +57,26 @@ public class IsoMessageLoggingHandler extends LoggingHandler {
         }
     }
 
-    private static String formatIsoMessage(IsoMessage m) {
+    private String formatIsoMessage(IsoMessage m) {
         StringBuilder sb = new StringBuilder();
-        sb.append("Message: ").append(m.debugString()).append("\n");
-        sb.append("TYPE: ").append(m.getType());
+        if (printSensitiveData) {
+            sb.append("Message: ").append(m.debugString()).append("\n");
+        }
+        sb.append("MTI: 0x").append(String.format("%04x", m.getType()));
         for (int i = 2; i < 128; i++) {
             if (m.hasField(i)) {
                 final IsoValue<Object> field = m.getField(i);
                 final Object objectValue = m.getObjectValue(i);
-                sb.append("F: ").append(i)
-                        .append("(").append(field.getType()).append("):")
+                sb.append("\n  ").append(i)
+                        .append(": [");
+
+                if (printFieldDescriptions) {
+                    sb.append(FIELD_NAMES[i - 1]).append(':');
+                }
+                sb.append(field.getType()).append('(').append(field.getLength())
+                        .append(")] = ")
                         .append(String.valueOf(objectValue))
-                        .append(" -> '").append(field.toString()).append("'\n");
+                        .append(" -> '").append(field.toString()).append('\'');
 
             }
         }
