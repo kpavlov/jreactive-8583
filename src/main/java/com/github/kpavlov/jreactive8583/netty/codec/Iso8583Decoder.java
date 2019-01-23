9a0910319334cd597472536a8863e1096b09ce4e
Karthik Velakur <karthik.velakur@hyperjar.com>
2019-01-23 13:29:08 -0500

    Log bytes in encoder and decoder

--- src/main/java/com/github/kpavlov/jreactive8583/netty/codec/Iso8583Decoder.java
+++ src/main/java/com/github/kpavlov/jreactive8583/netty/codec/Iso8583Decoder.java
@@ -2,16 +2,21 @@ package com.github.kpavlov.jreactive8583.netty.codec;
 
 import com.solab.iso8583.IsoMessage;
 import com.solab.iso8583.MessageFactory;
+import com.solab.iso8583.util.HexCodec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import org.slf4j.Logger;
 
 import java.text.ParseException;
 import java.util.List;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class Iso8583Decoder extends ByteToMessageDecoder {
 
     private final MessageFactory messageFactory;
+    private final Logger logger = getLogger(Iso8583Decoder.class);
 
     public Iso8583Decoder(MessageFactory messageFactory) {
         this.messageFactory = messageFactory;
@@ -29,6 +34,7 @@ public class Iso8583Decoder extends ByteToMessageDecoder {
         }
         byte[] bytes = new byte[byteBuf.readableBytes()];
         byteBuf.readBytes(bytes);
+        logger.debug("Received hex encoded bytes: {}", HexCodec.hexEncode(bytes, 0, bytes.length));
 
         final IsoMessage isoMessage = messageFactory.parseMessage(bytes, 0);
         if (isoMessage != null) {
--- src/main/java/com/github/kpavlov/jreactive8583/netty/codec/Iso8583Encoder.java
+++ src/main/java/com/github/kpavlov/jreactive8583/netty/codec/Iso8583Encoder.java
@@ -1,17 +1,22 @@
 package com.github.kpavlov.jreactive8583.netty.codec;
 
 import com.solab.iso8583.IsoMessage;
+import com.solab.iso8583.util.HexCodec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import org.slf4j.Logger;
 
 import java.nio.ByteBuffer;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @ChannelHandler.Sharable
 public class Iso8583Encoder extends MessageToByteEncoder<IsoMessage> {
 
     private final int lengthHeaderLength;
+    private final Logger logger = getLogger(Iso8583Encoder.class);
 
     public Iso8583Encoder(int lengthHeaderLength) {
         this.lengthHeaderLength = lengthHeaderLength;
@@ -21,9 +26,11 @@ public class Iso8583Encoder extends MessageToByteEncoder<IsoMessage> {
     protected void encode(ChannelHandlerContext ctx, IsoMessage isoMessage, ByteBuf out) {
         if (lengthHeaderLength == 0) {
             byte[] bytes = isoMessage.writeData();
+            logger.debug("Sending hex encoded bytes: {}", HexCodec.hexEncode(bytes, 0, bytes.length));
             out.writeBytes(bytes);
         } else {
             ByteBuffer byteBuffer = isoMessage.writeToBuffer(lengthHeaderLength);
+            logger.debug("Sending hex encoded bytes: {}", HexCodec.hexEncode(byteBuffer.array(), 0, byteBuffer.array().length));
             out.writeBytes(byteBuffer);
         }
     }
