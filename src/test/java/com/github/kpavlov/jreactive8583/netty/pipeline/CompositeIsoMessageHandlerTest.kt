858e1beb4d83992353684ea8a217d1827518813a
kpavlov <git@konstantinpavlov.net>
2020-04-15 09:30:59 +0300

    migrate to kotlin

--- src/test/java/com/github/kpavlov/jreactive8583/netty/pipeline/CompositeIsoMessageHandlerTest.kt
+++ src/test/java/com/github/kpavlov/jreactive8583/netty/pipeline/CompositeIsoMessageHandlerTest.kt
@@ -1,107 +1,110 @@
-package com.github.kpavlov.jreactive8583.netty.pipeline;
+package com.github.kpavlov.jreactive8583.netty.pipeline
 
-import com.github.kpavlov.jreactive8583.IsoMessageListener;
-import com.solab.iso8583.IsoMessage;
-import io.netty.channel.ChannelHandlerContext;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import com.github.kpavlov.jreactive8583.IsoMessageListener
+import com.solab.iso8583.IsoMessage
+import io.netty.channel.ChannelHandlerContext
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
 
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
-public class CompositeIsoMessageHandlerTest {
+@ExtendWith(MockitoExtension::class)
+class CompositeIsoMessageHandlerTest {
 
     @Mock
-    private IsoMessageListener<IsoMessage> listener1;
-    @Mock
-    private IsoMessageListener<IsoMessage> listener2;
-    @Mock
-    private IsoMessageListener<IsoMessage> listener3;
-    @Mock
-    private IsoMessage message;
-    @Mock
-    private ChannelHandlerContext ctx;
+    private lateinit var listener1: IsoMessageListener<IsoMessage>
 
-    private CompositeIsoMessageHandler<IsoMessage> handler = new CompositeIsoMessageHandler<>();
+    @Mock
+    private lateinit var listener2: IsoMessageListener<IsoMessage>
+
+    @Mock
+    private lateinit var listener3: IsoMessageListener<IsoMessage>
+
+    @Mock
+    private lateinit var message: IsoMessage
+
+    @Mock
+    private lateinit var ctx: ChannelHandlerContext
+    private var handler = CompositeIsoMessageHandler<IsoMessage>()
 
     @BeforeEach
-    public void setUp() {
-        handler.addListeners(listener1, listener2, listener3);
+    fun setUp() {
+        handler.addListeners(listener1, listener2, listener3)
     }
 
     @Test
-    public void shouldRemoveListener() throws Exception {
+    @Throws(Exception::class)
+    fun shouldRemoveListener() {
         //given
-        when(listener1.applies(message)).thenReturn(true);
-        when(listener3.applies(message)).thenReturn(true);
-        when(listener1.onMessage(ctx, message)).thenReturn(true);
+        Mockito.`when`(listener1.applies(message)).thenReturn(true)
+        Mockito.`when`(listener3.applies(message)).thenReturn(true)
+        Mockito.`when`(listener1.onMessage(ctx, message)).thenReturn(true)
 
         //when
-        handler.removeListener(listener2);
-        handler.channelRead(ctx, message);
+        handler.removeListener(listener2)
+        handler.channelRead(ctx, message)
 
         //then
-        verify(listener1).onMessage(ctx, message);
-        verify(listener2, never()).onMessage(ctx, message);
-        verify(listener3).onMessage(ctx, message);
+        Mockito.verify(listener1).onMessage(ctx, message)
+        Mockito.verify(listener2, Mockito.never()).onMessage(ctx, message)
+        Mockito.verify(listener3).onMessage(ctx, message)
     }
 
     @Test
-    public void shouldHandleWithAppropriateHandler() throws Exception {
+    @Throws(Exception::class)
+    fun shouldHandleWithAppropriateHandler() {
         //given
-        when(listener1.applies(message)).thenReturn(false);
-        when(listener2.applies(message)).thenReturn(true);
-        when(listener3.applies(message)).thenReturn(false);
-        when(listener2.onMessage(ctx, message)).thenReturn(true);
+        Mockito.`when`(listener1.applies(message)).thenReturn(false)
+        Mockito.`when`(listener2.applies(message)).thenReturn(true)
+        Mockito.`when`(listener3.applies(message)).thenReturn(false)
+        Mockito.`when`(listener2.onMessage(ctx, message)).thenReturn(true)
 
         //when
-        handler.channelRead(ctx, message);
+        handler.channelRead(ctx, message)
 
         //then
-        verify(listener1, never()).onMessage(ctx, message);
-        verify(listener2).onMessage(ctx, message);
-        verify(listener3, never()).onMessage(ctx, message);
+        Mockito.verify(listener1, Mockito.never()).onMessage(ctx, message)
+        Mockito.verify(listener2).onMessage(ctx, message)
+        Mockito.verify(listener3, Mockito.never()).onMessage(ctx, message)
     }
 
     @Test
-    public void testStopProcessing() throws Exception {
+    @Throws(Exception::class)
+    fun testStopProcessing() {
         //given
-        when(listener1.applies(message)).thenReturn(true);
-        when(listener2.applies(message)).thenReturn(true);
-        when(listener1.onMessage(ctx, message)).thenReturn(true);
-        when(listener2.onMessage(ctx, message)).thenReturn(false);
+        Mockito.`when`(listener1.applies(message)).thenReturn(true)
+        Mockito.`when`(listener2.applies(message)).thenReturn(true)
+        Mockito.`when`(listener1.onMessage(ctx, message)).thenReturn(true)
+        Mockito.`when`(listener2.onMessage(ctx, message)).thenReturn(false)
 
         //when
-        handler.channelRead(ctx, message);
+        handler.channelRead(ctx, message)
 
         //then
-        verify(listener1).onMessage(ctx, message);
-        verify(listener2).onMessage(ctx, message);
-        verify(listener3, never()).onMessage(ctx, message);
+        Mockito.verify(listener1).onMessage(ctx, message)
+        Mockito.verify(listener2).onMessage(ctx, message)
+        Mockito.verify(listener3, Mockito.never()).onMessage(ctx, message)
     }
 
     @Test
-    public void shouldNotFailOnExceptionInFailsafeMode() throws Exception {
+    @Throws(Exception::class)
+    fun shouldNotFailOnExceptionInFailsafeMode() {
         //given
-        handler = new CompositeIsoMessageHandler<>(false);
-        handler.addListeners(listener1, listener2, listener3);
-
-        when(listener1.applies(message)).thenReturn(true);
-        when(listener2.applies(message)).thenThrow(new RuntimeException("Expected exception"));
-        when(listener3.applies(message)).thenReturn(true);
-        when(listener1.onMessage(ctx, message)).thenReturn(true);
+        handler = CompositeIsoMessageHandler(false)
+        handler.addListeners(listener1, listener2, listener3)
+        Mockito.`when`(listener1.applies(message)).thenReturn(true)
+        Mockito.`when`(listener2.applies(message)).thenThrow(RuntimeException("Expected exception"))
+        Mockito.`when`(listener3.applies(message)).thenReturn(true)
+        Mockito.`when`(listener1.onMessage(ctx, message)).thenReturn(true)
 
         // when
-        handler.channelRead(ctx, message);
+        handler.channelRead(ctx, message)
 
         //then
-        verify(listener1).onMessage(ctx, message);
-        verify(listener2, never()).onMessage(ctx, message);
-        verify(listener3).onMessage(ctx, message);
+        Mockito.verify(listener1).onMessage(ctx, message)
+        Mockito.verify(listener2, Mockito.never()).onMessage(ctx, message)
+        Mockito.verify(listener3).onMessage(ctx, message)
     }
 }
