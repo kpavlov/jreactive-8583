4732b677e43b5935d8aab49ff3f61c10ad3f7968
Konstantin Pavlov <kpavlov@users.noreply.github.com>
2020-04-20 00:11:28 +0300

    Tests (#91)
    * Remove deprecated fields in ConnectorConfiguration
    * Fix possible NPE in client
    * Don't throw exception on server stop if channel is null
    * Test against OpenJDK 13 and OpenJDK 14

--- src/test/java/com/github/kpavlov/jreactive8583/it/ClientReconnectIT.java
+++ src/test/java/com/github/kpavlov/jreactive8583/it/ClientReconnectIT.java
@@ -1,25 +1,23 @@
-package com.github.kpavlov.jreactive8583.example;
+package com.github.kpavlov.jreactive8583.it;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.awaitility.Awaitility.await;
 
+@Tag("reconnect")
 public class ClientReconnectIT extends AbstractIT {
 
     @Test
     public void clientShouldReconnectWhenConnectionLost() throws Exception {
-        //given
-        await().alias("server started").until(server::isStarted);
-        await().alias("client connected").until(client::isConnected);
-
         //when
         server.shutdown();
         await().alias("client was disconnected").until(() -> (!client.isConnected()));
 
         //then
-        TimeUnit.SECONDS.sleep(7);
+        TimeUnit.SECONDS.sleep(3);
         server.init();
         server.start();
         await().alias("server started").until(server::isStarted);
