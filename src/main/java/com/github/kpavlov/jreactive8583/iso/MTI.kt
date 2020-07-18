e5df97b0d886710b4d240e08e1e822baecec0433
kpavlov <git@konstantinpavlov.net>
2020-07-19 02:32:29 +0300

    format

--- src/main/java/com/github/kpavlov/jreactive8583/iso/MTI.kt
+++ src/main/java/com/github/kpavlov/jreactive8583/iso/MTI.kt
@@ -5,12 +5,9 @@ package com.github.kpavlov.jreactive8583.iso
 object MTI {
 
     @JvmStatic
-    fun mtiValue(
-            iso8583Version: ISO8583Version,
-            messageClass: MessageClass,
-            messageFunction: MessageFunction,
-            messageOrigin: MessageOrigin
-    ): Int {
-        return iso8583Version.value() + messageClass.value() + messageFunction.value() + messageOrigin.value()
-    }
+    fun mtiValue(iso8583Version: ISO8583Version,
+                 messageClass: MessageClass,
+                 messageFunction: MessageFunction,
+                 messageOrigin: MessageOrigin) = iso8583Version.value() + messageClass.value() + messageFunction.value() + messageOrigin.value()
+
 }
