8291ebcee20c9d7a34746b031a9e3bebb8468195
kpavlov <{ID}+{username}@users.noreply.github.com>
2022-01-12 23:17:54 +0200

    disable kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,11 @@ plugins {
     kotlin("jvm") version "1.6.10"
     id("org.jetbrains.dokka") version "1.6.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
-    id("org.jetbrains.kotlinx.kover") version "0.4.4"
+//    id("org.jetbrains.kotlinx.kover") version "0.4.4"
 //    `maven-publish`
 }
 
 repositories {
-    google()
     mavenLocal()
     mavenCentral()
 }
