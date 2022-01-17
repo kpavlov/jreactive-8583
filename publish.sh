99fd2572eb65bc63446ca66467a50fa683badb23
kpavlov <{ID}+{username}@users.noreply.github.com>
2022-01-18 01:21:21 +0200

    Prepare to publish

--- publish.sh
+++ publish.sh
@@ -1,16 +1,20 @@
+# exit when any command fails
+set -e
 
 echo "Cleanup 🧹"
 rm -rf build
 
+#GRADLE_ARGS="-Pversion=1.3.5"
+
 echo "Building 📦"
 #./gradlew build --scan
-./gradlew build
+./gradlew $GRADLE_ARGS build
 
 echo "Testing 🧪"
-./gradlew check --stacktrace
+./gradlew $GRADLE_ARGS check --stacktrace
 
 echo "Publishing 📢"
 
 ## https://github.com/gradle-nexus/publish-plugin/
-./gradlew publishToSonatype
-#./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
+./gradlew $GRADLE_ARGS publishToSonatype
+#./gradlew $GRADLE_ARGS publishToSonatype closeAndReleaseSonatypeStagingRepository
