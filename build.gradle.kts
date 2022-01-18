3c7ac7ddc12991435dab29d332c7ace67a48eadb
Konstantin Pavlov <kpavlov@users.noreply.github.com>
2022-01-18 03:11:03 +0200

    - Bump dependencies (#139)
    - Upgrade netty to 4.1.73.Final
    - Upgrade slf4j-api to 1.7.33
    - Upgrade spring to 5.3.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,22 +19,22 @@ repositories {
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("net.sf.j8583:j8583:1.17.0")
-    implementation("io.netty:netty-handler:4.1.72.Final")
-    implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("com.google.code.findbugs:jsr305:3.0.2")
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    implementation("net.sf.j8583:j8583:1.17.0")
+    implementation("io.netty:netty-handler:4.1.73.Final")
+    implementation("org.slf4j:slf4j-api:1.7.33")
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
     testImplementation("org.mockito:mockito-junit-jupiter:4.2.0")
     testImplementation("org.apache.commons:commons-lang3:3.12.0")
     testImplementation("org.assertj:assertj-core:3.22.0")
-    testImplementation("org.springframework:spring-context:5.3.14")
-    testImplementation("org.springframework:spring-test:5.3.14")
+    testImplementation("org.springframework:spring-context:5.3.15")
+    testImplementation("org.springframework:spring-test:5.3.15")
     testImplementation("org.slf4j:slf4j-simple:1.7.32")
     testImplementation("net.jcip:jcip-annotations:1.0")
     testImplementation("org.awaitility:awaitility:4.1.1")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(kotlin("test-junit5"))
 }
 
 group = "com.github.kpavlov.jreactive8583"
