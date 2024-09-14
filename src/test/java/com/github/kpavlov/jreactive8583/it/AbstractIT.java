039e7185d5432e71ed176222a8fa9ad87e19796a
kpavlov <{ID}+{username}@users.noreply.github.com>
2024-09-14 12:38:02 +0300

    Remove dependency on jcip-annotations

--- src/test/java/com/github/kpavlov/jreactive8583/it/AbstractIT.java
+++ src/test/java/com/github/kpavlov/jreactive8583/it/AbstractIT.java
@@ -4,7 +4,6 @@ import com.github.kpavlov.jreactive8583.client.Iso8583Client;
 import com.github.kpavlov.jreactive8583.example.TestConfig;
 import com.github.kpavlov.jreactive8583.server.Iso8583Server;
 import com.solab.iso8583.IsoMessage;
-import net.jcip.annotations.NotThreadSafe;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,7 +15,6 @@ import static org.awaitility.Awaitility.await;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestConfig.class)
-@NotThreadSafe
 public abstract class AbstractIT {
 
     @Autowired
